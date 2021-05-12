938952865ff8485ca3c1f872e7f6e4c939b4329a
Suhas Jaybhaye <suhasjaybhaye.sj@gmail.com>
2021-05-12 14:39:10 +0530

    Add files via upload

---
+++ src/get_MQTT_data.py
@@ -0,0 +1,68 @@
+import paho.mqtt.client as mqtt
+from datetime import datetime
+import json
+import requests
+from requests.exceptions import HTTPError
+from pytz import timezone
+
+
+MQTT_ADDRESS = '192.168.43.162'
+MQTT_USER = 'pinkPanther'
+MQTT_PASSWORD = 'pinkPanther'
+MQTT_TOPIC = 'home/+/+'
+
+TS_API_CHWR_KEY = "IERXHH30U3GK21XU"
+TS_URL = "https://api.thingspeak.com/"
+CHANNEL_ID = "469519"
+
+def on_connect(client, userdata, flags, rc):
+    """ The callback for when the client receives a CONNACK response from the server."""
+    print('Connected with result code ' + str(rc))
+    client.subscribe(MQTT_TOPIC)
+
+
+def on_message(client, userdata, msg):
+    """The callback for when a PUBLISH message is received from the server."""
+    print(msg.topic + ' ' + str(msg.payload))
+    if msg.topic == 'home/data/json':
+        body = msg.payload
+        print ('Body : ',body)
+        dJSON = json.loads(body)
+        print ('...JSON: ', dJSON)
+        dJSON["created_at"] = datetime.now(timezone('Asia/Kolkata')).isoformat()
+
+        updates = []
+        updates.append(dJSON)
+
+        aJSON = updates
+        print("aJSON",aJSON)
+
+        headers = {"Content-type": "application/json"}
+        print(headers)
+        body = json.dumps({"write_api_key":TS_API_CHWR_KEY, "updates": aJSON})
+        print (body)
+        url = TS_URL + "channels/" + CHANNEL_ID + "/bulk_update.json"
+        print(url)
+        try:
+            r = requests.post(url, data=body, headers=headers, verify=True)
+            print(r.status_code)
+            r.raise_for_status()
+        except HTTPError as http_err:
+            print('HTTP error occurred:', http_err)
+        except Exception as err:
+            print('Other error occurred:', err)
+        else:
+            print('Success!')
+
+def main():
+    mqtt_client = mqtt.Client()
+    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    mqtt_client.connect(MQTT_ADDRESS, 1883)
+    mqtt_client.loop_forever()
+
+if __name__ == '__main__':
+    print('Into The Main')
+    main()
---
+++ src/main.cpp
@@ -0,0 +1,186 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include <ArduinoJson.h>
+
+#define DHTPIN 4
+
+#define DHTTYPE DHT11  // DHT11 or DHT22
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+
+const char* ssid = "Motorola_one_power_JS";
+const char* wifi_password = "Suhas@123";
+
+const char* mqtt_server = "192.168.43.162";  // IP of the MQTT broker
+const char* humidity_topic = "home/data/humidity";
+const char* temperature_topic = "home/data/temperature";
+const char* json_topic = "home/data/json";
+const char* mqtt_username = "pinkPanther"; // MQTT username
+const char* mqtt_password = "pinkPanther"; // MQTT password
+const char* clientID = "client_data"; // MQTT client ID
+
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
+
+#define lightCheck 32
+#define moistCheck 33
+#define Light 18
+#define Buzz 19
+#define Motor 21
+
+float tempC, humidity;
+int brightness, moisture;
+
+WiFiClient wifiClient;
+PubSubClient client(mqtt_server, 1883, wifiClient); 
+
+void connect_MQTT(){
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, wifi_password);
+
+  Serial.print("WiFi status: ");
+  Serial.println(WiFi.status());
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (client.connect(clientID, mqtt_username, mqtt_password)) {
+    Serial.println("Connected to MQTT Broker!");
+  }
+  else {
+    Serial.println("Connection to MQTT Broker failed...");
+  }
+}
+
+void setup() {
+  pinMode(lightCheck, INPUT);
+  pinMode(moistCheck, INPUT);
+  pinMode(DHTPIN, INPUT);
+  
+  pinMode(Light, OUTPUT);
+  pinMode(Buzz, OUTPUT);
+  pinMode(Motor, OUTPUT);
+  digitalWrite(Light, HIGH);
+  
+  Serial.begin(9600);
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+
+  connect_MQTT();  
+}
+
+
+void loop() {
+  connect_MQTT();
+  Serial.setTimeout(2000);
+  
+  DynamicJsonDocument doc(256);
+
+  Serial.println("______...______...______");
+
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature) || event.temperature > 100) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+    tempC =event.temperature;
+    doc["field1"] = tempC;
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity) || event.relative_humidity > 100) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+    humidity=event.relative_humidity;
+    doc["field2"] = humidity;
+  }
+
+  int moisture = analogRead(moistCheck) ;
+  Serial.print("Moisture = ");
+  Serial.println(moisture);
+  doc["field3"] = moisture;
+
+  int brightness = analogRead(lightCheck);
+  Serial.print("Brightness = ");
+  Serial.println(brightness);
+  doc["field4"] = brightness;
+  
+  serializeJson(doc, Serial);
+  Serial.println();
+
+  brightness > 600 ?
+    digitalWrite(Light, HIGH):
+    digitalWrite(Light, LOW);
+  
+  moisture < 50 ?
+    digitalWrite(Motor, HIGH):
+    digitalWrite(Motor, LOW);
+  
+  Serial.println(client.state ());
+  
+  char payload[128];
+  size_t payloadSize = serializeJson(doc, payload);
+
+  boolean ret;
+  if (client.state () == 0) {
+    client.beginPublish(json_topic, payloadSize, ret);
+    client.print(payload);
+    client.endPublish();
+    Serial.println("JSON sent!");
+  }
+  else {
+    Serial.println("JSON failed to send. Reconnecting to MQTT Broker and trying again");
+    client.connect(clientID, mqtt_username, mqtt_password);
+    delay(10); // This delay ensures that client.publish doesn't clash with the client.connect call
+    client.beginPublish(json_topic, payloadSize, ret);
+    client.print(payload);
+    client.endPublish();
+
+    Serial.println("JSON sent!");
+   }
+
+  // client.disconnect();  // disconnect from the MQTT broker
+  delay(1000*10);       // print new values every 1 Minute
+}
